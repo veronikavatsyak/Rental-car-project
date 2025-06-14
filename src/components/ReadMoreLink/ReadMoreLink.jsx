5ca76e91696f3754742957e3b361724451a1509c
veronikavatsyak <veronikateslya@ukr.net>
2025-06-13 22:24:30 -0400

    fixing catalog css

--- src/components/ReadMoreLink/ReadMoreLink.jsx
+++ src/components/ReadMoreLink/ReadMoreLink.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
+import css from './ReadMoreLink.module.css';
 
 const ReadMoreLink = () => {
   return (
-    <div>
+    <div className={css.linkWrap}>
       <Link className="link" to="/catalog/:id">
         Read More
       </Link>
